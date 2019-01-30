5af639139fdca2643558e294f3094f90413492b2
GregHilston <Gregory.Hilston@gmail.com>
2019-01-30 13:05:38 -0700

    Added notion of port for heroku deployment

--- app.py
+++ app.py
@@ -234,4 +234,6 @@ def step():
     return Response(json.dumps(d), 200, mimetype='text/json')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
